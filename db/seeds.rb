e9e7240c10a257128790cda849381db80b8b8210
joffrey <joffreycaugnon@gmail.com>
2021-11-08 18:53:59 +0100

    migrate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
+
+nb_users = 50
+nb_events = 10
+nb_attendances = 200
+
+Faker::Config.locale = 'fr'
+
+puts "Purge de la base de données..."
+
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
+puts "Début de l'insémination..."
+
+# Ajout de n User à la base de données
+nb_users.times do |i|
+  first_name = Faker::Games::Pokemon.name
+  last_name = Faker::JapaneseMedia::DragonBall.character
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    description: Faker::Hacker.say_something_smart ,
+    email: first_name + last_name.downcase + "@yopmail.com",
+    encrypted_password: first_name + last_name
+    )
+end
+
+puts "#{User.all.count} users sont en base de données"
+
+# Ajout de n Events à la base de données
+nb_events.times do
+  event = Event.create(
+  start_date: Faker::Time.forward,
+  title: Faker::Lorem.paragraph_by_chars(number: 15).chop,
+  description: Faker::Lorem.sentence(word_count: 50+ rand(10)),
+  duration: rand(1..100) * 5,
+  price: rand(10..100),
+  location: Faker::JapaneseMedia::Naruto.village,
+  admin: User.all.sample
+  )
+end
+puts "#{Event.all.count} évènements sont en base de données"
+
+nb_attendances.times do
+  Attendance.create!(
+    stripe_customer_id: Faker::Code.imei,
+    user: User.all.sample,
+    event: Event.all.sample
+  )
+end
+
+puts "#{Attendance.all.count} participations sont en base de données"
+
+
+puts "Fin du seed."
