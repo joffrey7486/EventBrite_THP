f71db55ab59977ab81533084cccda8d00bc42eb0
joffrey <joffreycaugnon@gmail.com>
2021-11-09 18:52:42 +0100

    modif event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,8 @@ class Event < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal: 1000 }
   validates :duration, presence: true, numericality: { greater_than: 0 }
   validate :is_five_multiplier
-  validates :start_date, presence: true
-  validates :location, presence: true
+  validates :start_date, presence: true, format: { with: ^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$ }
+  validates :location, presence: true 
   validate :end_after_start
 
   belongs_to :admin, class_name: "User"
@@ -23,3 +23,5 @@ class Event < ApplicationRecord
     errors.add(:duration, "must be a 5 multiplier") unless duration % 5 == 0
   end
 end
+
+^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$
