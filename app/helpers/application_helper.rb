915cdb510beea34792d64b45af8876e29af9688c
fleopaulD <f.devriese89@gmail.com>
2021-11-14 01:46:43 +0100

    add helper for event.duration display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def optimized_duration(duration)
+    if duration < 60
+      "#{duration} minutes"
+    elsif duration < 1440
+      "#{duration / 60} heures " + ("#{duration % 60} minutes" unless duration % 60 == 0)
+    else
+      days = duration / 1440
+      duration -= days * 1440
+      hours = duration / 60
+      "#{days} jours" + (" et #{hours} heures" unless hours == 0)
+    end
+  end
 end
